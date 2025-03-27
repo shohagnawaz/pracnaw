ecb068aa649f80c345b77d15846368e550abb6d5
shohagnawaz <snawazdigital@gmail.com>
2025-03-27 17:12:12 +0600

    banner

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This home page</h2>
+            <Banner></Banner>
         </div>
     );
 };
